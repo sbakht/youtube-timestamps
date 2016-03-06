f57a078fb65d360112831e45fdfa9cc32aa9a1af
Saad Bakht <rotk13@gmail.com>
2016-03-05 23:24:16 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name youStampApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the youStampApp
+ */
+angular.module('youStampApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name youStampApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the youStampApp
+ */
+angular.module('youStampApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
