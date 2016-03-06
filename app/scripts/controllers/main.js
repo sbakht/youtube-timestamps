92a1af30a7453d1cf57dcc8d87ee839b8cdabd57
Saad Bakht <rotk13@gmail.com>
2016-03-06 10:44:11 -0500

    get current video time in box when start typing

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,16 +9,24 @@
  */
 angular.module('youStampApp')
   .controller('MainCtrl', function ($scope) {
-  	$scope.theBestVideo = 'sMKoNBRZM1M';
+  	$scope.theBestVideo = 'm-CKVr6Z1Tw';
   	$scope.currentVideoTime = 0;
-  	
+  	$scope.newStampTime = 0;
+  	$scope.newStampInput = "";
+
   	$scope.$on('youtube.player.playing', function ($event, player) {
   		setInterval(function() { updateTimer(player) }, 500);
   	});
 
   	function updateTimer(player) {
-  		$scope.currentVideoTime = player.getCurrentTime();
+  		$scope.currentVideoTime = Math.floor(player.getCurrentTime());
   		console.log($scope.currentVideoTime);
+  	}
+
+  	$scope.keypress = function() {
+  		if($scope.newStampInput.length === 0) {
+  			$scope.newStampTime = $scope.currentVideoTime;
+  		}
   	}
 
   });
